d40e504ad8ef9629acc0554bac55702111e1c64d
itsdestroyer0 <120897603+itsdestroyer0@users.noreply.github.com>
2024-05-03 11:13:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,9 +29,6 @@ OWNER_ID = int(getenv("OWNER_ID", None))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TechPiroBots")
