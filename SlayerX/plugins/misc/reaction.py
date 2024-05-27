68db550620079c677bd4459ee3d176b0cc0a14b9
itsdestroyer0 <120897603+itsdestroyer0@users.noreply.github.com>
2024-05-27 11:38:27 +0530

    Update reaction.py

--- SlayerX/plugins/misc/reaction.py
+++ SlayerX/plugins/misc/reaction.py
@@ -5,8 +5,6 @@ from SlayerX import app
 
 emjs = ["😭", "❤️", "🥰", "👏", "🤬", "💋", "🔥", "👍", "🤔", "👀", "😁",]
 
-chat_id = message.chat.id
-
-@app.on_message(filters.chat(chat_id) & filters.text)
+@app.on_message(filters.chat(message.chat.id) & filters.text)
 async def x(_, m: Message):
     await app.send_reaction(m.chat.id, m.id, emoji=random.choice(emjs))
