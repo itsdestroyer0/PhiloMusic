8a3372811b5788e854ecdff85d7bcf84f075a479
itsdestroyer0 <120897603+itsdestroyer0@users.noreply.github.com>
2024-05-27 10:41:25 +0530

    Update reaction.py

--- SlayerX/plugins/misc/reaction.py
+++ SlayerX/plugins/misc/reaction.py
@@ -1,1 +1,11 @@
+from pyrogram import filters
+from pyrogram.types import Message
+import random
+from AnonXMusic import app
 
+emjs = ["😭", "❤️", "🥰", "👏", "🤬", "💋", "🔥", "👍", "🤔", "👀", "😁",]
+
+
+@app.on_message(filters.chat() & filters.text)
+async def x(_, m: Message):
+    await app.send_reaction(m.chat.id, m.id, emoji=random.choice(emjs))
